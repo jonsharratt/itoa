3b8984491f6bdbc12cdc1c01c65b705beef42082
Jon Sharratt <jon@craftship.io>
2022-11-06 15:05:34 +0000

    Setup test cases for itoa

---
+++ msstdlib.test/itoa.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "../msstdlib/msstdlib.h"
+TEST(MSStandardLib_itoa, Zero) {
+	char buffer[sizeof(int) * CHAR_BIT + 1];
+	char* expected = msstdlib::itoa(0, buffer);
+
+	EXPECT_EQ(std::string(expected), "0");
+}
+
+TEST(MSStandardLib_itoa, SignedZero) {
+	char buffer[sizeof(int) * CHAR_BIT + 1];
+	char* expected = msstdlib::itoa(-0, buffer);
+
+	EXPECT_EQ(std::string(expected), "0");
+}
+
+TEST(MSStandardLib_itoa, Unsigned) {
+	char buffer[sizeof(int) * CHAR_BIT + 1];
+	char* expected = msstdlib::itoa(123, buffer);
+
+	EXPECT_EQ(std::string(expected), "123");
+}
+
+TEST(MSStandardLib_itoa, Signed) {
+	char buffer[sizeof(int) * CHAR_BIT + 1];
+	char* expected = msstdlib::itoa(-123, buffer);
+
+	EXPECT_EQ(std::string(expected), "-123");
+}
